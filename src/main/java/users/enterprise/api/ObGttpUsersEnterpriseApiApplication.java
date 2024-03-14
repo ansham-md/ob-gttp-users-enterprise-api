a9bed4b6f7263eeaf1211a7ad5fd32974f565e6d
ansham-md <87389605+ansham-md@users.noreply.github.com>
2024-03-14 20:35:47 +0530

    Initial commit

---
+++ src/main/java/users/enterprise/api/ObGttpUsersEnterpriseApiApplication.java
@@ -0,0 +1,13 @@
+package users.enterprise.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObGttpUsersEnterpriseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObGttpUsersEnterpriseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/users/enterprise/api/ObGttpUsersEnterpriseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package users.enterprise.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObGttpUsersEnterpriseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
